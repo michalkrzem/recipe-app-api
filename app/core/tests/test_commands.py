fda59c5684e9cf16a54d8cc8e00c4a6f5e06dcb4
Michał Krzemiński <mkrzeminski@imgw.pl>
2022-09-01 11:21:34 +0200

    Add wait for db3

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,6 @@ from unittest.mock import patch
 # from psycopg2 import OperationalError as PsycopgError
 from psycopg2 import OperationalError as Psycopg2OpError
 
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
